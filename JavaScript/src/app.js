02a0485ae02df003b571cfc3121d467155a56389
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-08-28 13:09:55 +0530

    added actor route

--- JavaScript/src/app.js
+++ JavaScript/src/app.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express'
 import APIError from './utils/APIError.js';
 import movieRouter from './routes/movie.route.js';
+import actorRoute from './routes/actor.route.js';
 
 const app = express()
 app.use(json())
 app.use("/movie",movieRouter)
+app.use('/actor',actorRoute)
 app.use(APIError)
 export default app;
